37fc9231451047bc9d2a385634b7efb0cfbdda36
Theo <theo.piedade@gmail.com>
2023-09-25 16:36:37 -0300

    games controller OK

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -17,15 +17,15 @@ export async function getGames(req, res) {
 export async function postGames(req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body;
 
-    //const checkName = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
-    //if (checkName) return res.status(409);
+    const checkName = await db.query(`SELECT id FROM games WHERE "name" = $1;`, [name]);
+    if (checkName.rows[0]) return res.sendStatus(409);
 
     try{
 
-        const games = await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`,
         [name, image, stockTotal, pricePerDay]);
 
-        return res.status(201);
+        return res.sendStatus(201);
 
     }catch(err){
         return res.status(500).send(err.message);
