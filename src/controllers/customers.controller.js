de073b5842963414b69ccc186f7675cfc128f713
Theo <theo.piedade@gmail.com>
2023-09-25 17:45:26 -0300

    Customers

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -17,15 +17,15 @@ export async function getCustomers(req, res) {
 export async function createCustomers(req, res) {
     const { name, phone, cpf, birthday  } = req.body;
 
-    //const checkName = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
-    //if (checkName) return res.status(409);
+    const checkCPF = await db.query(`SELECT * FROM customers WHERE "cpf" = $1;`, [cpf]);
+    if (checkCPF.rows[0]) return res.sendStatus(409);
 
     try{
 
         const customers = await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
         [name, phone, cpf, birthday]);
 
-        return res.status(201);
+        return res.sendStatus(201);
 
     }catch(err){
         return res.status(500).send(err.message);
@@ -33,17 +33,34 @@ export async function createCustomers(req, res) {
 
 }
 
+export async function getCustomersById(req, res) {
+    const { id } = req.params;
+
+    try{
+
+        const customers = await db.query(`SELECT * FROM customers WHERE "id" = ($1);`,
+        [id]);
+
+        if (customers.rows[0]) return res.send(customers.rows);
+        else return res.sendStatus(404);
+
+    }catch(err){
+        return res.status(500).send(err.message);
+    }
+
+}
 
 export async function updateCustomers(req, res) {
+    const { id } = req.params;
     const { name, phone, cpf, birthday  } = req.body;
 
-    //const checkName = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
-    //if (checkName) return res.status(409);
+    const checkCpfId = await db.query(`SELECT id FROM customers WHERE "cpf" = $1;`, [cpf]);
+    if (checkCpfId && checkCpfId != id) return res.sendStatus(409);
 
     try{
 
-        const customers = await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
-        [name, phone, cpf, birthday]);
+        await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE "id" = VALUES ($5);`,
+        [name, phone, cpf, birthday, id]);
 
         return res.status(201);
 
@@ -52,3 +69,4 @@ export async function updateCustomers(req, res) {
     }
 
 }
+
